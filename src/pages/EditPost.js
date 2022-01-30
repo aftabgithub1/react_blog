4af1ce44693beb04668143693d222d61f0c4137c
Aftab <aftab2060@gmail.com>
2022-01-31 03:41:16 +0600

    edited

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -35,7 +35,7 @@ const EditPost = () => {
       setPosts(listPosts);
       setEditPostTitle('');
       setEditPostBody('');
-      navigate('/react_blog/');
+      navigate('/react_blog');
     } catch(err) {
       console.log(err.message);
     }
