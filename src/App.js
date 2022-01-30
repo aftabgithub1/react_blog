ef7b4658d95b378381c7b9598bd7536fef226920
Aftab <aftab2060@gmail.com>
2022-01-31 00:49:40 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -26,11 +26,11 @@ const App = () => {
             <Routes>
               {/* <Route index element={ <Home /> } /> */}
               <Route path="/" exact element={ <Home /> } />
-              <Route path="/post" element={ <NewPost /> } />
-              <Route path="/post/:id/:postTitle" element={ <PostPage /> } />
-              <Route path="/edit-post/:id" element={ <EditPost /> } />
-              <Route path="/about" element={<About />} />
-              <Route path="/test" element={<Test />} />
+              <Route path="./post" element={ <NewPost /> } />
+              <Route path="./post/:id/:postTitle" element={ <PostPage /> } />
+              <Route path="./edit-post/:id" element={ <EditPost /> } />
+              <Route path="./about" element={<About />} />
+              <Route path="./test" element={<Test />} />
               <Route path="*" element={<MissingPage />} />
             </Routes>
           </DataProvider>
