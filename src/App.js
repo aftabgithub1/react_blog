807d6f73bbaff582a5fb75e07e764dcae6caf35e
Aftab <aftab2060@gmail.com>
2022-01-31 02:19:00 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 
 // layouts
@@ -6,37 +6,18 @@ import Header from './Header';
 import Nav from './Nav';
 import Footer from './Footer';
 
-// pages
-import Home from './pages/Home';
-import NewPost from './pages/NewPost';
-import PostPage from './pages/PostPage';
-import About from './pages/About';
-import EditPost from './pages/EditPost';
-import Test from './pages/Test';
-import MissingPage from './pages/MissingPage';
 
 
 const App = () => {
   return (
-    <Router>
       <div className='app'>
         <Header />
           <DataProvider>
             <Nav />
-            <Routes>
-              {/* <Route index element={ <Home /> } /> */}
-              <Route path="./" exact element={ <Home /> } />
-              <Route path="./post" element={ <NewPost /> } />
-              <Route path="./post/:id/:postTitle" element={ <PostPage /> } />
-              <Route path="./edit-post/:id" element={ <EditPost /> } />
-              <Route path="./about" element={<About />} />
-              <Route path="./test" element={<Test />} />
-              <Route path="*" element={<MissingPage />} />
-            </Routes>
+            <Outlet />
           </DataProvider>
         <Footer />
       </div>
-    </Router>
   );
 } 
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import NewPost from './pages/NewPost';
+import PostPage from './pages/PostPage';
+import About from './pages/About';
+import EditPost from './pages/EditPost';
+import Test from './pages/Test';
+import MissingPage from './pages/MissingPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={ <App /> }>
+          <Route index element={ <Home /> } />
+          <Route path="post" element={ <NewPost /> } />
+          <Route path="post/:id/:postTitle" element={ <PostPage /> } />
+          <Route path="edit-post/:id" element={ <EditPost /> } />
+          <Route path="about" element={<About />} />
+          <Route path="test" element={<Test />} />
+          <Route path="*" element={<MissingPage />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
